d7d856196c58915e7b5bccf6ade5724579698998
Albert Chu <albertzyk@gmail.com>
2014-06-05 17:16:12 +0800

    Initial Commit

---
+++ ACSJSONPathTests/ACSJSONPathTests.swift
@@ -0,0 +1,35 @@
+//
+//  ACSJSONPathTests.swift
+//  ACSJSONPathTests
+//
+//  Created by Albert Chu on 14-6-5.
+//  Copyright (c) 2014年 Albert Chu. All rights reserved.
+//
+
+import XCTest
+
+class ACSJSONPathTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
