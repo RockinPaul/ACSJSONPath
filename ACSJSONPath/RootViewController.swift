51958c4daf9b25304de35739ecabefc08c9a76d7
Albert Chu <albertzyk@gmail.com>
2014-06-06 22:36:38 +0800

    network

--- ACSJSONPath/RootViewController.swift
+++ ACSJSONPath/RootViewController.swift
@@ -8,9 +8,23 @@
 
 import UIKit
 
-class RootViewController: UIViewController {
+class RootViewController: UIViewController, UIAlertViewDelegate {
+    
+    //
+    var myButton: UIButton?
+    var alert: UIAlertView?
+    
     
 //-- Action Methods --------------------------------------------------------------------------------
+    
+    func buttonClick(sender: UIButton!) {
+        gettingOLJSONData()
+    }
+    
+    // alert item
+    func alertView(alertView: UIAlertView!, clickedButtonAtIndex buttonIndex: Int) {
+        println("buttonIndex: \(buttonIndex) clicked")
+    }
     
 //------------------------------------------------------------------------------------------------//
 
@@ -25,15 +39,57 @@ class RootViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
-        gettingJSONData()
+        gettingLocalJSONData()
         
+        setUpView()
     }
     
     
+    func setUpView() {
+        
+        //-- button --------------------------------------------------------------------------------
+        self.myButton = UIButton(frame:CGRect(origin: CGPointMake(60.0, 200.0), size: CGSizeMake(200,50)))
+        self.myButton!.setTitle("button", forState: UIControlState.Normal)
+        
+        var buttonBGColor : UIColor = UIColor(red: 0.8, green: 0.9, blue: 1.0, alpha: 1)
+        var borderColor : UIColor = UIColor(red: 0, green: 0.3, blue: 0.6, alpha: 1)
+        
+        self.myButton!.backgroundColor = buttonBGColor
+        self.myButton!.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        
+        var highLightColor : UIColor = UIColor(red: 0.5, green: 0.5, blue: 0.6, alpha: 1)
+        self.myButton!.setTitleColor(highLightColor, forState: UIControlState.Highlighted)
+        
+        // border
+        self.myButton!.layer.cornerRadius = 8
+        self.myButton!.layer.borderWidth = 1
+        self.myButton!.layer.borderColor = borderColor.CGColor
+        
+        
+        self.myButton!.addTarget(self, action: "buttonClick:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(self.myButton)
+        //----------------------------------------------------------------------------------------//
+        
+        //-- alert ---------------------------------------------------------------------------------
+        // this crashes
+        //self.alert = UIAlertView(title: "alert", message: "this is an alert", delegate: self, cancelButtonTitle: "cancel")
+        
+        self.alert = UIAlertView()
+        
+        
+        self.alert!.delegate = self
+        self.alert!.addButtonWithTitle("cancel")
+        
+        //self.alert!.title = "alert"
+        //self.alert!.message = "this is an alert"
+        //----------------------------------------------------------------------------------------//
+
+    }
     
-//-- test Methods ----------------------------------------------------------------------------------
     
-    func gettingJSONData() {
+//-- local JSON file data --------------------------------------------------------------------------
+    
+    func gettingLocalJSONData() {
         
         var jsonUtil = ACSJSONUtil.sharedInstance
         
@@ -52,7 +108,7 @@ class RootViewController: UIViewController {
                 
                 var jsonPath = ACSJSONPath.sharedInstance
                 var jsonData : AnyObject? = jsonPath.getDataFromJSONObject(responseJSON, by: "template.test[1][0].one[0][0].te")
-                println(jsonData)
+                println("data from local JSON path is: \(jsonData)")
                 
             },
             
@@ -63,6 +119,62 @@ class RootViewController: UIViewController {
         
     }
     
-//--------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------//
+    
+    
+//-- JSON data OL ----------------------------------------------------------------------------------
+    func gettingOLJSONData() {
+        
+        var responseObj: AnyObject?
+        
+        var theURL: NSURL = NSURL(string: "http://www.battlenet.com.cn/api/wow/battlePet/species/258")
+        
+        var request: NSMutableURLRequest = NSMutableURLRequest(
+            URL: theURL,
+            cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy,
+            timeoutInterval: 10)
+        
+        request.HTTPMethod = "GET"
+        
+        NSURLConnection.sendAsynchronousRequest(
+            
+            request,
+            
+            queue:NSOperationQueue.mainQueue(),
+            
+            completionHandler: {(response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+                
+                if !error {
+                    // 请求成功，处理返回结果
+                    var responseString: NSString = NSString(data: data, encoding:NSUTF8StringEncoding)
+                    
+                    var data: NSData = responseString.dataUsingEncoding(NSUTF8StringEncoding)
+                    
+                    var json : AnyObject! =
+                    NSJSONSerialization.JSONObjectWithData(
+                        data,
+                        options:NSJSONReadingOptions.AllowFragments,
+                        error:nil)
+                    
+                    var dict = json as NSDictionary
+                    var jsonPath = ACSJSONPath.sharedInstance
+                    
+                    var name: AnyObject? = jsonPath.getDataFromJSONObject(dict, by: "name")
+                    var abilitie01: AnyObject? = jsonPath.getDataFromJSONObject(dict, by: "abilities[0].name")
+                    
+                    //println(dict)
+                    
+                    self.alert!.title = "对战宠物名称: \(name as String)"
+                    self.alert!.message = "技能1: \(abilitie01 as String)"
+                    self.alert!.show()
+                }
+                
+            }
+            
+        )
+
+    }
+    
+//------------------------------------------------------------------------------------------------//
 
 }
